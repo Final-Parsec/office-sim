334e4ae22a922d900c9c588c8bfe7e051fdd5855
Baer <baer.bradford@gmail.com>
2023-09-06 21:02:35 -0500

    render a red square

--- office-sim.js
+++ office-sim.js
@@ -11,4 +11,13 @@ function main() {
 
   canvasContext.clearColor(0.0, 0.0, 0.0, 1.0);
   canvasContext.clear(canvasContext.COLOR_BUFFER_BIT);
+
+  let boxSize = 100;
+  canvasContext.viewport(0, 0, canvasContext.drawingBufferWidth, canvasContext.drawingBufferHeight);
+  canvasContext.enable(canvasContext.SCISSOR_TEST);
+  let x = canvasContext.drawingBufferWidth / 2 - boxSize / 2;
+  let y = canvasContext.drawingBufferHeight / 2 - boxSize / 2;
+  canvasContext.scissor(x, y, boxSize, boxSize);
+  canvasContext.clearColor(1.0, 0.0, 0.0, 1.0);
+  canvasContext.clear(canvasContext.COLOR_BUFFER_BIT);
 }
