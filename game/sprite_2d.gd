ae4ff62135201c9c5d9cbb3b76667a07b3e8bf44
Baer <baer.bradford@gmail.com>
2025-01-12 09:51:30 -0600

    control rotation and position with input

--- game/sprite_2d.gd
+++ game/sprite_2d.gd
@@ -4,7 +4,15 @@ var speed = 400
 var angular_speed = PI
 
 func _process(delta: float):
-	rotation += angular_speed * delta
+	var direction = 0
+	if Input.is_action_pressed("ui_left"):
+		direction = -1
+	if Input.is_action_pressed("ui_right"):
+		direction = 1
 	
-	var velocity = Vector2.UP.rotated(rotation) * speed
+	rotation += angular_speed * delta * direction
+	
+	var velocity = Vector2.ZERO
+	if Input.is_action_pressed("ui_up"):
+		velocity = Vector2.UP.rotated(rotation) * speed
 	position += velocity * delta
