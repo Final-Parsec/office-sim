dbace29a6cbafec7d2b54f799d609d79f080e8ff
Baer <baer.bradford@gmail.com>
2025-02-12 16:22:14 -0600

    [FP-702] Office furniture (#10)
    * workbench art and the beginnings of an action bar
    
    * clean up input so that gui handles with priority
    
    * making action bar affect a game state and then using that game state
    
    * place furniture when you're in furniture mode
    
    * tweak scales a bit

---
+++ game/enums.gd
@@ -0,0 +1,7 @@
+extends Node
+
+enum Actions {
+	NONE = 0,
+	WIDGET = 1,
+	FURNITURE = 2
+}
--- game/player.gd
+++ game/player.gd
@@ -3,6 +3,8 @@ extends CharacterBody2D
 @export var speed = 400
 @export var widget_scene: PackedScene
 @export var widget_container: Node2D
+@export var furniture_scene: PackedScene
+@export var furniture_container: Node2D
 var screen_size
 
 func _ready() -> void:
@@ -34,28 +36,36 @@ func start(pos):
 	show()
 	$CollisionShape2D.disabled = false
 	
-func _input(event):
+func _unhandled_input(event: InputEvent) -> void:
 	if event.is_action_released("perform_action"):
-		# Build Widget Action
-		var clicked_a_widget = false
-		var clicked_ineligible_placement = false
-		
-		# Determine if you are clicking existing widget.
-		for widget in widget_container.get_children():
-			var collision_shape = widget.get_node("CollisionShape2D") as CollisionShape2D
-			var circle = collision_shape.shape as CircleShape2D
-			var radius = circle.radius
-			if event.position.distance_to(collision_shape.global_position) <= radius && !clicked_a_widget:
-				clicked_a_widget = true
-				widget.build(10)
-			if event.position.distance_to(collision_shape.global_position) <= radius * 2:
-				clicked_ineligible_placement = true
-		
-		# If not, place a new widget.
-		if !clicked_ineligible_placement:
-			place_widget(event.position)
+		if GameState.selected_action == Enums.Actions.WIDGET:
+			# Build Widget Action
+			var clicked_a_widget = false
+			var clicked_ineligible_placement = false
+			
+			# Determine if you are clicking existing widget.
+			for widget in widget_container.get_children():
+				var collision_shape = widget.get_node("CollisionShape2D") as CollisionShape2D
+				var circle = collision_shape.shape as CircleShape2D
+				var radius = circle.radius
+				if event.position.distance_to(collision_shape.global_position) <= radius && !clicked_a_widget:
+					clicked_a_widget = true
+					widget.build(10)
+				if event.position.distance_to(collision_shape.global_position) <= radius * 2:
+					clicked_ineligible_placement = true
+			
+			# If not, place a new widget.
+			if !clicked_ineligible_placement:
+				place_widget(event.position)
+		if GameState.selected_action == Enums.Actions.FURNITURE:
+			place_furniture(event.position)
 	
 func place_widget(spawn_location: Vector2):
 	var widget = widget_scene.instantiate()
 	widget.position = spawn_location
 	widget_container.add_child(widget)
+
+func place_furniture(spawn_location: Vector2):
+	var furniture = furniture_scene.instantiate()
+	furniture.position = spawn_location
+	furniture_container.add_child(furniture)
