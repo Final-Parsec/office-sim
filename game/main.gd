1f4b7e4117e4dcdebbcdc0befe29085f3e0fd666
Baer <baer.bradford@gmail.com>
2025-02-13 11:30:08 -0600

    move furniture placement into main and decrement net worth when placing furniture

--- game/main.gd
+++ game/main.gd
@@ -2,6 +2,9 @@ extends Node
 
 var net_worth
 
+@export var furniture_scene: PackedScene
+@export var furniture_container: Node2D
+
 func game_over() -> void:
 	$ScoreTimer.stop()
 	$HUD.show_game_over()
@@ -15,3 +18,18 @@ func new_game():
 	$HUD.update_net_worth(net_worth)
 	$HUD.show_message("Rise & Grind")
 	#$Music.play()
+
+
+func _on_player_furniture_placed() -> void:
+	net_worth -= 100
+	$HUD.update_net_worth(net_worth)
+
+
+func _on_player_furniture_placement_requested(position: Vector2) -> void:
+	var FURNITURE_COST = 100
+	if net_worth >= FURNITURE_COST:
+		net_worth -= FURNITURE_COST
+		$HUD.update_net_worth(net_worth)
+		var furniture = furniture_scene.instantiate()
+		furniture.position = position
+		furniture_container.add_child(furniture)
