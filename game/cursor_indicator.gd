32bc6dd111e34e67397e06a3da83003bf5d9cf0b
Baer <baer.bradford@gmail.com>
2025-02-24 09:25:47 -0600

    refactor / reparent

--- game/cursor_indicator.gd
+++ game/cursor_indicator.gd
@@ -8,7 +8,7 @@ func _ready() -> void:
 func _process(_delta: float) -> void:
 	if GameState.selected_action == Enums.Actions.WIDGET:
 		global_position = get_viewport().get_mouse_position()
-		visible = global_position.distance_to(get_parent().position) <= 100
+		visible = global_position.distance_to($"../Player".position) <= 100
 
 func _on_hud_action_bar_button_pressed() -> void:
 	if GameState.selected_action == Enums.Actions.WIDGET:
